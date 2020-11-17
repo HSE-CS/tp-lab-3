9f0027ebac21c41dc72fe2ddd06644d034da6e8c
jswark <swark.a.j@gmail.com>
2020-11-18 02:24:30 +0300

    task1

---
+++ include/task1.h
@@ -0,0 +1,7 @@
+#ifndef TASK1_TASK1_H
+#define TASK1_TASK1_H
+
+double calcDelta(); // расчет зазора между землей и веревкой
+double calcCost(); // расчет стоимости бассейна
+
+#endif //TASK1_TASK1_H
