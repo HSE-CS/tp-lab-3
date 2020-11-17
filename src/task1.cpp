11fbf7a3a68a7c386756b48514c819c9995b3bdb
Egor Trukhin <estrukhin@gmail.com>
2020-11-17 20:50:58 +0300

    calcDelta() fix

--- src/task1.cpp
+++ src/task1.cpp
@@ -2,7 +2,7 @@
 #include "Circle.h"
 
 double calcDelta() {
-	Circle earth(6378100.0); // meters
+	Circle earth(6378.1); // kilometers
 	Circle rope;
 	rope.setFerence(earth.getFerence() + 1.0);
 	return rope.getRadius() - earth.getRadius();
