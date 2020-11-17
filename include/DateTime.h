3164f9c174b9f4db80f33f2221fcf7bd021ce46c
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2020-11-17 12:31:40 +0300

    created DateTime class

--- include/DateTime.h
+++ include/DateTime.h
@@ -5,4 +5,28 @@
 #ifndef TASK1_DATETIME_H
 #define TASK1_DATETIME_H
 
-#endif //TASK1_DATETIME_H
+#include <string>
+
+class DateTime {
+private:
+
+public:
+
+    DateTime();
+
+    DateTime(DateTime &datetime);
+
+    std::string getToday();
+
+    std::string getYesterday();
+
+    std::string getTomorrow();
+
+    DateTime getFuture(unsigned int N);
+
+    DateTime getPast(unsigned int N);
+
+    DateTime getDifference(DateTime &);
+};
+
+#endif
--- src/DateTime.cpp
+++ src/DateTime.cpp
@@ -3,3 +3,37 @@
  */
 
 #include "../include/DateTime.h"
+
+DateTime::DateTime() {
+
+}
+
+DateTime::DateTime(DateTime &datetime) {
+
+}
+
+std::string DateTime::getToday() {
+    return std::string();
+}
+
+std::string DateTime::getYesterday() {
+    return std::string();
+}
+
+std::string DateTime::getTomorrow() {
+    return std::string();
+}
+
+DateTime DateTime::getFuture(unsigned int N) {
+    return DateTime();
+}
+
+DateTime DateTime::getPast(unsigned int N) {
+    return DateTime();
+}
+
+DateTime DateTime::getDifference(DateTime &) {
+    return DateTime();
+}
+
+
