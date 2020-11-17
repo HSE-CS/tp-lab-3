1daf98c544d6aaba3090ae2e750a4fed1bd0cee1
KuznetsovMikhail <KuznetsovMikhail>
2020-11-17 22:29:41 +0300

    add task1

--- include/task1.h
+++ include/task1.h
@@ -1,4 +1,9 @@
 #ifndef TASK1_H_INCLUDED
 #define TASK1_H_INCLUDED
 
+#include "Circle.h"
+
+double calcDelta();
+double calcCost();
+
 #endif // TASK1_H_INCLUDED
--- src/task1.cpp
+++ src/task1.cpp
@@ -1,1 +1,31 @@
+#include "task1.h"
 
+double calcDelta()
+{
+    const double earthRadius = 6378.1;
+    double elong = 0.001;
+
+    Circle circle(earthRadius);
+    circle.setFerence(circle.getFerence() + elong);
+
+    const double metrInKm = 1000;
+    double delta = (circle.getRadius() - earthRadius) * metrInKm;
+    return delta;
+}
+
+double calcCost()
+{
+    const double poolRad = 3, fenceRad = 4;
+
+    Circle circle(poolRad);
+    double poolArea = circle.getArea();
+
+    circle.setRadius(fenceRad);
+    double fenceArea = circle.getArea();
+    double fenceFer = circle.getFerence();
+
+    const double fencePr = 2000, trackPr = 1000;
+    double totalPr = (fenceArea - poolArea) * trackPr ;
+    totalPr += fenceFer * fencePr;
+    return totalPr;
+}
