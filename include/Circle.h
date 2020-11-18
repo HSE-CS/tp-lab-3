3c408de400487a5ba0dc80fcb3bd02059fe44032
Pasha Emshanov <63115710+Dev-will-work@users.noreply.github.com>
2020-11-18 14:39:36 +0300

    added methods declarations, variables, removed destructor in Circle.h

--- include/Circle.h
+++ include/Circle.h
@@ -3,9 +3,16 @@
 class Circle
 {
 private:
-
+	double radius = 0;
+	double ference = 0;
+	double area = 0;
 public:
-	Circle();
-	~Circle();
+	Circle(double);
+	void setRadius(double);
+	void setFerence(double);
+	void setArea(double);
+	double getRadius();
+	double getFerence();
+	double getArea();
 };
 #endif
