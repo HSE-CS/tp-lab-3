cb91e2317f6e0b9b35ff4c6ce8bc4c20f816df8e
Pasha Emshanov <63115710+Dev-will-work@users.noreply.github.com>
2020-11-18 15:17:25 +0300

    declarations and definiton of calcDelta with their dependencies

--- include/task1.h
+++ include/task1.h
@@ -1,4 +1,7 @@
 #ifndef TASK1_H
 #define TASK1_H
 
+double calcDelta(); //- расчет зазора между землей и веревкой
+double calcCost(); //-расчет стоимости бассейна
+
 #endif
--- src/task1.cpp
+++ src/task1.cpp
@@ -1,2 +1,15 @@
 #include "../include/task1.h"
+#include "../include/Circle.h"
 
+double calcDelta() {
+	Circle earth(6378.1);
+	double earth_radius = earth.getRadius();
+	earth.setFerence(earth.getFerence() + 1);
+	double new_radius = earth.getRadius();
+	double delta = new_radius - earth_radius;
+	return delta;
+}//- расчет зазора между землей и веревкой
+
+double calcCost() {
+	return 0.0;
+}//-расчет стоимости бассейна
