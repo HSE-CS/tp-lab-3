b84e9c7dac829419b03a35a2e89470b9b006eb9f
ArtemDanyaev <ardan2076@mail.ru>
2020-11-18 21:51:49 +0300

    first task done

---
+++ src/main1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include "task1.h"
+
+using namespace std;
+
+int main()
+{
+    double t1 = calcDelta();
+    cout << setprecision(10) << t1;
+}
---
+++ src/main2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+#include "task1.h"
+
+using namespace std;
+
+int main()
+{
+    cout << setprecision(10) << calcCost();
+}
