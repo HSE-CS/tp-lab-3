c2d2bb0c00cfef50f89b39ba974a6fc1b4a8761b
Pasha Emshanov <63115710+Dev-will-work@users.noreply.github.com>
2020-11-18 19:10:14 +0300

    declaration of DateTime methods and its variables

--- include/DateTime.h
+++ include/DateTime.h
@@ -1,12 +1,22 @@
 #ifndef DATETIME_H
 #define DATETIME_H
+#include <ctime>
+#include <string>
 class DateTime
 {
 private:
-
+	time_t time_in_sec;
+	tm timetable;
 public:
+	DateTime(int, int, int);
 	DateTime();
-	~DateTime();
+	DateTime(const DateTime&);
+	std::string getToday(); //- возвращение текущей даты в виде строки, с указанием дня недели и названия месяца(например 07 november 2018, wedensday);
+	std::string getYesterday(); //- возвращение даты вчерашнего дня в виде строки.
+	std::string getTomorrow(); //- возвращение даты завтрашнего дня в виде строки.
+	time_t getFuture(unsigned int N); //- возвращение даты через N дней в будущем;
+	time_t getPast(unsigned int N); //- возвращение даты через N дней в прошлом;
+	time_t getDifference(DateTime&); //- для расчёта разницы(в днях) между двумя датами
 
 };
 #endif
