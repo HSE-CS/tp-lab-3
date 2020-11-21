c16c3ade9af2931bdaaa0bc32dee8e19126d8f24
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2020-11-21 11:30:04 +0300

    increase calculus accuracy

--- src/Circle.cpp
+++ src/Circle.cpp
@@ -6,18 +6,18 @@
 
 void Circle::setRadius(double radius) {
     this->radius = radius;
-    this->ference = 2 * M_PI * radius;
+    this->ference = 2.0 * M_PI * radius;
     this->area = M_PI * radius * radius;
 }
 
 void Circle::setFerence(double ference) {
     this->ference = ference;
-    this->radius =  ference / (2 * M_PI);
-    this->area = (ference * ference) / ( 4 * M_PI);
+    this->radius = ference / (2.0 * M_PI);
+    this->area = M_PI * radius * radius;
 }
 
 void Circle::setArea(double area) {
-    this->area = area;
     this->radius = sqrt(area / M_PI);
-    this->ference = sqrt(4 * M_PI * area);
+    this->ference = 2.0 * M_PI * this->radius;
+    this->area = area;
 }
