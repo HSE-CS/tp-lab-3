cb020cae049b8a8177e35f35abfb1fe6bb14237e
danek0100 <71225718+danek0100@users.noreply.github.com>
2020-11-22 12:47:24 +0300

    1 task

--- src/task1.cpp
+++ src/task1.cpp
@@ -1,12 +1,19 @@
 #include "Circle.h"
+#include "task1.h"
 
 using namespace std;
 
-// расчёт зазора между землёй и верёвкой
 double calcDelta()
 {
-	return 0;
+	Circle Earth(637810.0); // в метрах
+	Circle rope(637810.0);
+	cout << "Enter the length of the piece of rope added (metres):" << endl;
+	double add;
+	cin >> add;
+	rope.setFerence(rope.getFerence() + add);
+	return (rope.getRadius() - Earth.getRadius());
 }
+
 // расчёт стоимости бассейна
 double calcCost()
 {
