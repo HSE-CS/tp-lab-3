e450e5246be505d143142b94a125305b5ad14e9f
ivanxcc <djsupersky@gmail.com>
2020-11-22 19:29:06 +0300

    Add task1 solution

---
+++ src/task1.cpp
@@ -0,0 +1,15 @@
+#include "task1.h"
+#include "Circle.h"
+
+double calcDelta() {
+	Circle earth = Circle(6378.1);
+	earth.setFerence(earth.getFerence() + 1.0);
+	return earth.getRadius() - 6378.1;
+}
+
+double calcCost() {
+	Circle pool = Circle(3);
+	Circle poolPlusWalkWay = Circle(4);
+	double concreteWalkwayArea = poolPlusWalkWay.getArea() - pool.getArea();
+	return concreteWalkwayArea * 1000.0 + pool.getFerence() * 2000.0;
+}
