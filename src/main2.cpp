2d2482c201d7f8eab75d15093c23aa7e7a278d3c
ivanxcc <djsupersky@gmail.com>
2020-11-22 19:31:38 +0300

    Add task1 solution in main

---
+++ src/main2.cpp
@@ -0,0 +1,13 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include "Circle.h"
+#include "task1.h"
+#include "DateTime.h"
+#include <ctime>
+
+
+int main()
+{
+	std::cout << calcCost() << std::endl;
+	return 0;
+}
