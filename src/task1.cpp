6c2c234ccaf0c402525356aedf553bdc4b2f11d0
Dolgopolov Alexey <62442582+LexaLexarLexarex@users.noreply.github.com>
2020-11-22 20:33:14 +0300

    Update task1.cpp

--- src/task1.cpp
+++ src/task1.cpp
@@ -2,13 +2,13 @@
 #include "Circle.h"
 
 double calcDelta() {
-	Circle Earth(6378.1);
+	Circle Earth(6378100.0);
 	Circle loop;
-	loop.setFerence(Earth.getFerence() + 0.001);
+	loop.setFerence(Earth.getFerence() + 1.0);
 	return loop.getRadius() - Earth.getRadius();
 }
 double calcCost() {
 	Circle pool(3.0);
 	Circle poolBet(pool.getRadius() + 1.0);
-	return pool.getFerence() * 2000 + (poolBet.getArea() - pool.getArea()) * 1000;
+	return poolBet.getFerence() * 2000 + (poolBet.getArea() - pool.getArea()) * 1000;
 }
