e03bc077d2a33d96d41e67c043b32b6dcde48917
Shaarlotka <tytylik14@gmail.com>
2020-11-23 16:39:18 +0300

    task1 done

---
+++ include/Circle.h
@@ -0,0 +1,22 @@
+#ifndef CIRCLE_H
+#define CIRCLE_H
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+class Circle {
+private:
+	double radius;
+	double ference;
+	double area;
+public:
+	Circle(double radius);
+	void setRadius(double radius);
+	void setFerence(double ference);
+	void setArea(double area);
+	double getRadius() const;
+	double getFerence() const;
+	double  getArea() const;
+};
+
+#endif
