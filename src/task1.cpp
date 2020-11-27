47df98fb8c93d2415adfc1a63546e462b13e1577
Olesya Nikolaeva <63335416+olesyani@users.noreply.github.com>
2020-11-27 18:24:10 +0300

    who knows..

--- src/task1.cpp
+++ src/task1.cpp
@@ -2,15 +2,12 @@
 #include "Circle.h"
 
 double calcDelta() {
-    Circle Earth(0), Rope(0);
-    Earth.setRadius(6381.1);
+    Circle Earth(6381.1), Rope(0);
     Rope.setFerence(Earth.getFerence() + 1);
-    return Rope.getRadius() - Earth.getRadius();
+    return Rope.getRadius() - 6381.1;
 }
 
 double calcCost() {
-    Circle Pool(0), Road(0);
-    Pool.setRadius(3);
-    Road.setRadius(4);
+    Circle Pool(3), Road(4);
     return 1000*(Road.getArea() - Pool.getArea()) + 2000*Road.getFerence();
 }
