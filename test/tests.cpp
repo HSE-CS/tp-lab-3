2c21915de66c04658f43cc646ba4dc8714c11f35
danek0100 <71225718+danek0100@users.noreply.github.com>
2020-11-27 15:02:35 +0300

    edit test

--- test/tests.cpp
+++ test/tests.cpp
@@ -59,7 +59,7 @@ TEST(date,datetime_getdiff2)
 }
 TEST(calc,delta)
 {    
-    EXPECT_DOUBLE_EQ(0.1591549430,calcDelta());
+    EXPECT_DOUBLE_EQ(0.15915494307409972,calcDelta());
 }
 TEST(calc,pool_cost)
 {    
