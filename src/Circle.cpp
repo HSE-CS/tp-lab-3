52394b65a2e5f832d4b4695cedf5302d62a5df6d
m_khorosh <marina.khoroshilova@gmail.com>
2020-11-27 18:08:31 +0300

    correct pi constant

--- src/Circle.cpp
+++ src/Circle.cpp
@@ -1,7 +1,7 @@
 #include "Circle.h"
+#define _USE_MATH_DEFINES
 #include <math.h>
 
-double pi = 2 * acos(0.0);
 
 Circle::Circle() {
     setRadius(0);
@@ -13,20 +13,20 @@ Circle::Circle(double r) {
 
 void Circle::setRadius(double r){
     radius = r;
-    ference = 2 * pi * radius;
-    area = pi * radius * radius;
+    ference = 2 * M_PI * radius;
+    area = M_PI * radius * radius;
 }
 
 void Circle::setFerence(double f){
     ference = f;
-    radius = ference/(2 * pi);
-    area = pi * radius * radius;
+    radius = ference/(2 * M_PI);
+    area = M_PI * radius * radius;
 }
 
 void Circle::setArea(double a){
     area = a;
-    radius = sqrt(area / pi);
-    ference = 2 * pi * radius;
+    radius = sqrt(area / M_PI);
+    ference = 2 * M_PI * radius;
 }
 
 double Circle::getRadius(){
