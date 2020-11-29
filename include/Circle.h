8b731e5f83b3eb03a8ed21f2c05dbd7af8533a49
Roma Balayan <roma1balayan@gmail.com>
2020-11-29 14:51:44 +0300

    pi changed v2

--- include/Circle.h
+++ include/Circle.h
@@ -6,13 +6,13 @@ private:
 	double radius;
 	double ference;
 	double area;
-	//double pi = 3.1415926535897933;
-	double pi = M_PI;
+	//double M_PI = 3.1415926535897933;
+	//double M_PI = M_PI;
 public:
 	Circle(double rad) {
 		this->radius = rad;
-		this->ference = 2 * pi * this->radius;
-		this->area = pi * this->radius * this->radius;
+		this->ference = 2 * M_PI * this->radius;
+		this->area = M_PI * this->radius * this->radius;
 	}
 	void setRadius(double rad);
 	void setFerence(double fer);
--- src/Circle.cpp
+++ src/Circle.cpp
@@ -4,22 +4,22 @@
 void Circle::setRadius(double rad)
 {
 	this->radius = rad;
-	this->ference = 2 * pi * this->radius;
-	this->area = pi * this->radius * this->radius;
+	this->ference = 2 * M_PI * this->radius;
+	this->area = M_PI * this->radius * this->radius;
 }
 
 void Circle::setFerence(double fer)
 {
 	this->ference = fer;
-	this->radius = this->ference / (2 * pi);
-	this->area = pi * this->radius * this->radius;
+	this->radius = this->ference / (2 * M_PI);
+	this->area = M_PI * this->radius * this->radius;
 }
 
 void Circle::setArea(double area)
 {
 	this->area = area;
-	this->radius = sqrt(this->area / (pi));
-	this->ference = 2 * pi * this->radius;
+	this->radius = sqrt(this->area / (M_PI));
+	this->ference = 2 * M_PI * this->radius;
 }
 
 double Circle::getRadius()
