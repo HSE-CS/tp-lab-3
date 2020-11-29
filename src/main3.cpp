117e6676137567aae61e390644a6e85cb868ca45
__dolt_coder__ <ihackit595@gmail.com>
2020-11-29 11:13:27 +0300

    fix output for days number less 10

--- src/main3.cpp
+++ src/main3.cpp
@@ -6,9 +6,12 @@
 int main() {
     DateTime d2 = DateTime(2, 12, 2020);
     DateTime d = DateTime();
+    DateTime dt(31,10,2018);
+
     std::cout <<d.getToday() <<std::endl;
     std::cout <<d2.getYesterday() <<std::endl;
+    std::cout <<dt.getTomorrow() <<std::endl;
     std::cout <<d.getPast(2) << std::endl;
-    std::cout <<d.getDifference(d2)<<std::endl;
+    std::cout <<d2.getDifference(d)<<std::endl;
     return 0;
 }
