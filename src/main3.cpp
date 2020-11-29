57e12eecc62c3475661e7fcfed680a9911058002
Ivan Dobryaev <ivan.dobryaev@gmail.com>
2020-11-29 13:41:49 +0300

    Why do I have sementation falut on unit test?

--- src/main3.cpp
+++ src/main3.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 int main() {
-	DateTime Now;
-	cout << Now.getToday() << '\n';
-  cout << Now.getTomorrow() << '\n';
-  cout << Now.getYesterday() << '\n';
-	cout << Now.getFuture(10) << '\n';
-  cout << Now.getPast(23) << '\n';
-	DateTime Date(18, 12, 2020);
-	cout << Now.getDifference(Date) << '\n';
+	// DateTime Now;
+	// cout << Now.getToday() << '\n';
+  // cout << Now.getTomorrow() << '\n';
+  // cout << Now.getYesterday() << '\n';
+	// cout << Now.getFuture(10) << '\n';
+  // cout << Now.getPast(23) << '\n';
+	// DateTime Date(18, 12, 2020);
+	// cout << Now.getDifference(Date) << '\n';
 
 }
