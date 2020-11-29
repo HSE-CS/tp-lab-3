70bc990daf0244385b47d2d00a84727d4b585bb9
Ilya <ilya.tatsenko@yandex.ru>
2020-11-29 18:45:08 +0300

    last

--- src/task1.cpp
+++ src/task1.cpp
@@ -8,7 +8,7 @@ double calcDelta()
 	Circle c1(6378.1), c2(0);
 	double l = c1.getFerence();
 	c2.setFerence((l + 0.001));
-	return ((c2.getRadius() - c1.getRadius())) * 1000.0;
+	return long double((c2.getRadius() - c1.getRadius())) * 1000.0;
 
 
 }
@@ -22,7 +22,7 @@ double calcCost()
 	dorosh.setArea((dorosh.getArea() - bas.getArea()));
 	st_dorosh = dorosh.getArea() * 1000.0;
 	st_zabora = 2000.0 * zabor.getFerence();
-	return st_dorosh + st_zabora;
+	return long double(st_dorosh + st_zabora);
 
 
 
