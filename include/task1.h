3cbfb7ff5991b412a000dd50edf31cdb5dfd08ea
ArinaMonicheva <63364781+ArinaMonicheva@users.noreply.github.com>
2020-11-29 14:14:20 +0300

    Update task1.h

--- include/task1.h
+++ include/task1.h
@@ -2,7 +2,7 @@
 #define TASK1_H
 #include "Circle.h"
 
-double calcDelta(int piece = 1, double EarthRadius = 6378100.0);
+double calcDelta(int piece = 1, double EarthRadius = 6378.1);
 double calcCost(double poolRadius = 3.0, int concretePrice = 1000,
 		int fencePrice = 2000, double roadWidth = 1.0);
 
