3f0194f57cc8788eb5119b96de73c5d310db6c46
GDM1705 <71228914+GDM1705@users.noreply.github.com>
2020-11-29 18:41:27 +0300

    Update main3.cpp

--- src/main3.cpp
+++ src/main3.cpp
@@ -1,21 +1,21 @@
-#include "DataTime.h"
+#include "DateTime.h"
 #include <iostream>
 
 using namespace std;
 
 int main() {
-	DataTime dt(31, 10, 2018);
+	DateTime dt(31, 10, 2018);
 	cout << dt.getToday() << endl;
 	cout << "01 november 2018, thursday   -  " << dt.getTomorrow() << endl;
-	DataTime dt2(1, 11, 2018);
+	DateTime dt2(1, 11, 2018);
 	cout << "02 november 2018, friday  -   " << dt2.getTomorrow() << endl;
-	DataTime dt3(1, 11, 2018);
+	DateTime dt3(1, 11, 2018);
 	cout << "01 november 2018, thursday   -  " << dt3.getToday() << endl;
-	DataTime dt4(20, 11, 2018);
-	DataTime dt5(20, 11, 2018);
+	DateTime dt4(20, 11, 2018);
+	DateTime dt5(20, 11, 2018);
 	cout << 0 << " - " << dt4.getDifference(dt5) << endl;
-	DataTime dt6(20, 11, 2018);
-	DataTime dt7(21, 11, 2018);
+	DateTime dt6(20, 11, 2018);
+	DateTime dt7(21, 11, 2018);
 
 	cout << 1 << " - " << dt7.getDifference(dt6) << endl;
 
