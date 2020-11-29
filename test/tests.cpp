2b96e5ed085d7a0fd0d35f67a53c176b53c7c0f7
Ivan Dobryaev <ivan.dobryaev@gmail.com>
2020-11-29 23:24:03 +0300

    make another accuracy

--- test/tests.cpp
+++ test/tests.cpp
@@ -59,9 +59,9 @@ TEST(date,datetime_getdiff2)
 }
 TEST(calc,delta)
 {
-    EXPECT_DOUBLE_EQ(0.15915494309228961,calcDelta());
+    EXPECT_DOUBLE_EQ(0.159,calcDelta());
 }
 TEST(calc,pool_cost)
 {
-    EXPECT_DOUBLE_EQ(72256.63103256523,calcCost());
+    EXPECT_DOUBLE_EQ(72256.63,calcCost());
 }
