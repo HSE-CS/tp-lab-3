210c74a3662c0cc44c832031a2e636ca9221146a
Leonidek23 <rad.shabarshin@mail.ru>
2020-11-29 17:44:39 +0300

    круг

---
+++ include/Circle.h
@@ -0,0 +1,24 @@
+#include <math.h>
+
+class Circle {
+private:
+    double radius;
+    double ference;
+    double area;
+public:
+    Circle(double radius = 0) {
+        setRadius(radius);
+    }
+
+    void setRadius(double radius);
+
+    void setFerence(double ference);
+
+    void setArea(double area);
+
+    double getRadius();
+
+    double getFerence();
+
+    double getArea();
+};
