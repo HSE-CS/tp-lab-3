acc8f139a8f3d8165ffb25ff2a5c623dc4fd0511
Shaidi <drsharibzhanova@edu.hse.ru>
2020-12-05 18:29:03 +0300

    Circle tasks 2/ forgot main2

---
+++ src/main2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "task1.h"
+#include "Circle.h"
+
+int main() {
+
+	Circle pool(3);
+	Circle hedge(4);
+	std::cout << "The cost of the work is" << calcCost << std::endl;
+	return 0;
+}
+
