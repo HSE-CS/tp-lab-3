57e5a839e874d89289d2bb7739585f84384e1ff9
Alexander Grachev <s89519011368@yandex.ru>
2020-12-06 23:29:08 +0300

    DateTime Header

--- include/DateTime.h
+++ include/DateTime.h
@@ -1,6 +1,21 @@
 #ifndef TASK1_DATETIME_H
 #define TASK1_DATETIME_H
 
+using namespace std;
 
+class DateTime{
+private:
+    time_t infotime;
+public:
+    DateTime(size_t day, size_t month, size_t year);
+    DateTime();
+    DateTime(DateTime&);
+    string getToday();
+    string getYesterday();
+    string getTomorrow();
+    string getFuture(unsigned int N);
+    string getPast(unsigned int N);
+    double getDifference(DateTime&);
+};
 
 #endif //TASK1_DATETIME_H
