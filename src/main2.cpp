37b0307554ed3f0c1f4fc0e81d23e3f4d278e853
Alexander Grachev <s89519011368@yandex.ru>
2020-12-06 23:22:59 +0300

    Task 1 done

--- src/main2.cpp
+++ src/main2.cpp
@@ -1,1 +1,8 @@
+#include "../include/task1.h"
+#include <iostream>
+
+int main(){
+    std::cout << calcCost() << std::endl;
+    return 0;
+}
 
--- src/task1.cpp
+++ src/task1.cpp
@@ -1,1 +1,18 @@
+#include "../include/task1.h"
+#include "../include/Circle.h"
+
+double calcDelta(){
+    Circle earth;
+    earth.setRadius(6378.1);
+    earth.setFerence(earth.getFerence() + 1);
+    return earth.getRadius() - 6378.1;
+}
+
+double calcCost(){
+    Circle pool;
+    pool.setRadius(3.0);
+    Circle place;
+    place.setRadius(4.0);
+    return (place.getArea() - pool.getArea()) * 1000 + place.getFerence() * 2000;
+}
 
