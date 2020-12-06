ad23fb449c2a9235e1b127108e4edd79f27e77de
Alexander Grachev <s89519011368@yandex.ru>
2020-12-06 22:12:19 +0300

    class Circle

--- include/Circle.h
+++ include/Circle.h
@@ -1,6 +1,18 @@
 #ifndef TASK1_CIRCLE_H
 #define TASK1_CIRCLE_H
 
-
+class Circle{
+private:
+    double radius;
+    double ference;
+    double area;
+public:
+    void setRadius();
+    void setFerence();
+    void setArea();
+    double getRadius();
+    double getFerence();
+    double getArea();
+};
 
 #endif //TASK1_CIRCLE_H
