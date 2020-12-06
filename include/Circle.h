35ad630e71f3681b385befa5c2f077688413c516
Shaidi <drsharibzhanova@edu.hse.ru>
2020-12-06 14:11:58 +0300

    Maybe

--- include/Circle.h
+++ include/Circle.h
@@ -1,15 +1,17 @@
+#ifndef CIRCLE_H
+#define CIRCLE_H
 #include <iostream>
 #include <cmath>
 #include <math.h>
-#define _USE_MATH_DEFINES 
+#define _USE_MATH_DEFINES
 
 #define M_PI 3.14159265358979323846
 
 class Circle
 {
 private:
-	double radius; 
-	double ference; 
+	double radius;
+	double ference;
 	double area;
 	double ference_from_radius(double r);
 	double area_from_radius(double r);
@@ -21,10 +23,11 @@ public:
 		ference = ference_from_radius(radius);
 		area = area_from_radius(radius);
 	}
-	void setRadius(double); 
-	void setFerence(double); 
-	void setArea(double); 
+	void setRadius(double);
+	void setFerence(double);
+	void setArea(double);
 	double getRadius();
 	double getFerence();
 	double getArea();
 };
+#endif
