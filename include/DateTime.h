b6fe59244f2cc4a0cd3b3e9b80b33d74c5a82a8f
Alexander Grachev <s89519011368@yandex.ru>
2020-12-06 23:41:50 +0300

    DateTime Header changes

--- include/DateTime.h
+++ include/DateTime.h
@@ -1,15 +1,31 @@
 #ifndef TASK1_DATETIME_H
 #define TASK1_DATETIME_H
+#include <iostream>
 
 using namespace std;
 
 class DateTime{
 private:
-    time_t infotime;
+    time_t timeInf;
 public:
-    DateTime(size_t day, size_t month, size_t year);
-    DateTime();
-    DateTime(DateTime&);
+    DateTime(size_t day, size_t month, size_t year){
+        timeInf = time(nullptr);
+        struct tm *struct_time = localtime(&timeInf);
+
+        struct_time->tm_mday = day;
+        struct_time->tm_mon = month - 1;
+        struct_time->tm_year = year - 1900;
+        timeInf = mktime(struct_time);
+    }
+
+    DateTime() {
+        timeInf = time(nullptr);
+    }
+
+    DateTime(DateTime &oldDate){
+        timeInf = oldDate.timeInf;
+    }
+
     string getToday();
     string getYesterday();
     string getTomorrow();
---
+++ src/DateTime.cpp
@@ -0,0 +1,27 @@
+#include "../include/DateTime.h"
+
+using namespace std;
+
+string getToday(){
+
+}
+
+string getYesterday(){
+
+}
+
+string getTomorrow(){
+
+}
+
+string getFuture(unsigned int N){
+
+}
+
+string getPast(unsigned int N){
+
+}
+
+double getDifference(DateTime&){
+
+}
