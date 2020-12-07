02300fe6447049b50474671b7c180c0f7eb47a5d
skprpi <fatin.maksim@gmail.com>
2020-12-07 16:17:46 +0300

    Fixed

--- src/task1.cpp
+++ src/task1.cpp
@@ -5,7 +5,7 @@ double calcCost() {
     Circle p(3);
     double f_r = p.getRadius() + 1;
     Circle l(f_r);
-    Circle l(f_r);
+    Circle f(f_r);
     return (lane.getArea() - p.getArea()) * 1000 + f.getFerence() * 2000;
 }
 
