5686ff28eb6e5199964b6e120483a2c7ea178824
Alexander Grachev <s89519011368@yandex.ru>
2020-12-16 15:41:27 +0300

    task1 changed

--- src/task1.cpp
+++ src/task1.cpp
@@ -2,17 +2,14 @@
 #include "../include/Circle.h"
 
 double calcDelta(){
-    Circle earth;
-    earth.setRadius(6378.1);
+    Circle earth(6378.1);
     earth.setFerence(earth.getFerence() + 1);
     return earth.getRadius() - 6378.1;
 }
 
 double calcCost(){
-    Circle pool;
-    pool.setRadius(3.0);
-    Circle place;
-    place.setRadius(4.0);
+    Circle pool(3.0);
+    Circle place(4.0);
     return (place.getArea() - pool.getArea()) * 1000 + place.getFerence() * 2000;
 }
 
